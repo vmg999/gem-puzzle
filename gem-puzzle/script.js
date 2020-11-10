4d418855b278cef8ac6e6e1d565e9020c9dcac96
vmg999 <vmg999@gmail.com>
2020-11-10 23:32:57 +0200

    feat: add checkWin function

--- gem-puzzle/script.js
+++ gem-puzzle/script.js
@@ -5,15 +5,20 @@ const puzzle = {
         tiles: [],
         emptyTile: {
             top: null,
-            left: null
+            left: null,
+            position: null,
         }
     },
     parameters:{
         puzzleSize: 4,
         puzzleBoxSize: 400,
     },
+    counter:{
+        moves: 0,
+        time: 0,
+    },
 
-    init(){
+    init(s=4){
         const body = document.querySelector(".body");
         this.elements.main = document.createElement("div");
         this.elements.main.classList.add("main");
@@ -24,17 +29,30 @@ const puzzle = {
         body.append(this.elements.main);
         this.elements.main.append(this.elements.pzl);
 
+        //----------размер поля-----------
+        if(s < 3){
+            this.parameters.puzzleSize = 3;
+        }else if(s > 8){
+            this.parameters.puzzleSize = 8;
+        }else{
+            this.parameters.puzzleSize = s;
+        }
+        this.parameters.puzzleBoxSize = this.parameters.puzzleSize * 100;
+        document.styleSheets[0].cssRules[1].style.cssText=`--size: ${this.parameters.puzzleBoxSize}px;`;
+
         this.createTiles();
         this.placeTiles();
+
         
         
     },
 
     createTiles(){
-        const arr = this.createArr(this.parameters.puzzleSize);
+        const arr = this.createArr();
         arr.forEach(el=>{
             const tile = document.createElement("div");
             tile.classList.add("tile");
+            tile.classList.add(`tilesize${this.parameters.puzzleSize}`);
             tile.innerHTML = el;
             tile.left = 0;
             tile.top = 0;
@@ -43,7 +61,6 @@ const puzzle = {
                 tile.empty = true;
             }
             this.elements.tiles.push(tile);
-            // this.elements.pzl.append(tile);
         });
 
     },
@@ -54,11 +71,14 @@ const puzzle = {
         this.elements.tiles.forEach((el, key)=>{
             el.top = Math.floor(key / this.parameters.puzzleSize)*tileSize;
             el.left = (key % this.parameters.puzzleSize)*tileSize;
+            el.position = key + 1;
             el.style.left = el.left + "px";
             el.style.top = el.top + "px";
             if(el.empty){
                 this.elements.emptyTile.top = el.top;
                 this.elements.emptyTile.left = el.left;
+                this.elements.emptyTile.position = key + 1;
+                el.position = 0;
             }
             el.addEventListener("click", ()=>{
                 this.moveTile(key);
@@ -71,6 +91,8 @@ const puzzle = {
     moveTile(k){
         x = this.elements.tiles[k].left;
         y = this.elements.tiles[k].top;
+        p = this.elements.tiles[k].position;
+
         tileSize = this.parameters.puzzleBoxSize / this.parameters.puzzleSize;
 
         if((Math.abs(x - this.elements.emptyTile.left) + Math.abs(y - this.elements.emptyTile.top)) == tileSize){
@@ -78,22 +100,26 @@ const puzzle = {
             this.elements.tiles[k].style.left = this.elements.emptyTile.left + "px";
             this.elements.tiles[k].top = this.elements.emptyTile.top;
             this.elements.tiles[k].style.top = this.elements.emptyTile.top + "px";
+            this.elements.tiles[k].position = this.elements.emptyTile.position;
 
             this.elements.emptyTile.left = x;
             this.elements.emptyTile.top = y;
+            this.elements.emptyTile.position = p;
+
+            this.counter.moves++;
+            this.checkWin();
         }
     },
 
-    
-    createArr(size=4){ // массив с рандомными числами
-        if(size < 3){
-            size = 3;
-        }else if(size > 8){
-            size = 8;
+    checkWin(){
+        if(this.elements.tiles.every(el=>el.innerHTML == el.position)){
+            setTimeout(()=>alert("win"), 300);
         }
-        
+    },
+    
+    createArr(){ // массив с рандомными числами
         let puzzle=[];
-        for(let i=0;i<size**2;i++) puzzle.push(i);
+        for(let i=0;i<this.parameters.puzzleSize**2;i++) puzzle.push(i);
         puzzle = puzzle.sort(()=>Math.random()-0.5);
         
         return puzzle;
@@ -102,5 +128,5 @@ const puzzle = {
 
 
 
-puzzle.init();
+puzzle.init(4);
 
